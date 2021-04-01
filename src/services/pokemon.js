c78ecd8088a833213774ec569f5d2bd7c14e2444
k-marczak <k_marczak@outlook.com>
2021-04-01 16:50:40 +0200

    display all pokemon with all the abilities, css left

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -4,6 +4,17 @@ export async function getAllPokemon(url) {
             .then(res => res.json())
             .then(data => {
                 resolve(data);
+            });
+    });
+}
+
+
+export async function getPokemon(url) {
+    return new Promise ((resolve, reject) => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                resolve(data);
             })
     })
 }
